1c23b8c67d74b99e06e71346eaebbb18eefa18ad
doberste8 <doberste8@gmail.com>
2017-05-12 16:54:36 -0400

    added ggplot

--- elo.R
+++ elo.R
@@ -1,8 +1,9 @@
 library(dplyr)
+library(ggplot2)
 
 x <- seq(-5,5,length=100)
 plot(x,type="l")
 f <- function(x) {
     return(x^2)
 }
-plot(f(x), type="l", lwd=2, col="blue")
+qplot(x,f(x))+theme_dark()
