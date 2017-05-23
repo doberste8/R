f5d34532aa5b780863e8eb709c6fde88d1d188c9
doberste8 <doberste8@gmail.com>
2017-05-23 17:08:40 -0400

    added parallel processing for fitness functions. added weighted_fitness function.

--- castles.R
+++ castles.R
@@ -1,5 +1,15 @@
 library(readr)
+library(parallel)
+
+# Calculate the number of cores
+no_cores <- detectCores() - 1
+
+# Initiate cluster
+cl <- makeCluster(no_cores)
+
 castle_solutions <- read_csv("castle-solutions.csv")
+
+clusterExport(cl, "castle_solutions")
 
 par(mfrow = c(2, 5))  # 3 rows and 2 columns
 for (i in c(1:10)) {
@@ -7,7 +17,7 @@ for (i in c(1:10)) {
 }
 
 fitness <- function(genome) {
-    score <- vector("numeric",nrow(castle_solutions));
+    score <- vector("numeric",nrow(castle_solutions))
     for (j in 1:nrow(castle_solutions)) {
         match_score <- 0;
     for (i in 1:length(genome)) {
@@ -25,12 +35,40 @@ fitness <- function(genome) {
     return(wp)
 }
 
+weighted_fitness <- function(genome) {
+    score <- vector("numeric",nrow(castle_solutions))
+    for (j in 1:nrow(castle_solutions)) {
+        match_score <- 0;
+        for (i in 1:length(genome)) {
+            if (genome[i] > castle_solutions[j,i]) {
+                match_score <- match_score + i 
+            } else {
+                if (genome[i] == castle_solutions[j,i]) {
+                    match_score <- match_score + i*.5 
+                }
+            }
+        }
+        score[j] <- match_score*castle_solutions[j,12]^2
+    }
+    return(Reduce("+",score))
+}
+
+castle_solutions$wp <- parApply(cl, castle_solutions[,1:10],1,fitness)
+clusterExport(cl, "castle_solutions")
+castle_solutions$wpw <- parApply(cl, castle_solutions[,1:10],1,weighted_fitness)
+stopCluster(cl)
+
 g <- c(3,5,8,10,13,1,26,30,2,2)
 print(fitness(g))
+print(weighted_fitness(g))
 
 genome <- function() {
     t <- sample(1:10,100,replace = T)
     return(table(t))
 }
 t <- data.frame(t(replicate(3,genome())))
-t$max <- apply(t,1,fitness)
+t$wp <- apply(t,1,fitness)
+
+par(mfrow = c(1, 1))  # 3 rows and 2 columns
+hist(castle_solutions$wp, prob='T', breaks=50)
+plot(castle_solutions$wp,castle_solutions$wpw)
